512ff861488e87869c5d72a3c5db1b1178025fcd
Samuel Elliott <samuel@fancy.org.uk>
2023-01-12 00:30:55 +0000

    Use nxapi server class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import * as os from 'node:os';
 import { fileURLToPath } from 'node:url';
 import { Storage, LocalStorageProvider, Users, ErrorResponse, version, addUserAgent } from 'nxapi';
-import { SplatNet3 } from 'nxapi/splatnet3';
+import { RequestIdSymbol, SplatNet3 } from 'nxapi/splatnet3';
+import createDebug from 'debug';
 import { DownloadSearchReplayResult, RequestId } from 'splatnet3-types/splatnet3';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import persist from 'node-persist';
 
 interface ReplayData {
@@ -12,66 +13,130 @@ interface ReplayData {
     created_at: number;
 }
 
-const product = 'splatoon3-replay-lookup/0.1.0';
+const debug = createDebug('splatoon3-replay-lookup');
+
+const product = 'splatoon3-replay-lookup/0.2.0';
 addUserAgent(product);
-
-const storage = await Storage.create(LocalStorageProvider, new URL('../data', import.meta.url));
-const users = new Users(storage, process.env.ZNC_PROXY_URL);
-const splatnet = await users.get(SplatNet3, process.env.NA_USER_ID!);
-
-const app = express();
-
-const cache = persist.create({
-    dir: fileURLToPath(new URL('../data/persist', import.meta.url)),
-    stringify: data => JSON.stringify(data, null, 4) + '\n',
-});
-await cache.init();
-
-const replays = new Map<string, Promise<ReplayData>>();
 
 const REPLAY_CODE_REGEX = /^[A-Z0-9]{16}$/;
 
-app.use('/api/splatnet3', (req, res, next) => {
-    console.log('[%s] %s %s HTTP/%s from %s, port %d%s, %s',
-        new Date(), req.method, req.url, req.httpVersion,
-        req.socket.remoteAddress, req.socket.remotePort,
-        req.headers['x-forwarded-for'] ? ' (' + req.headers['x-forwarded-for'] + ')' : '',
-        req.headers['user-agent']);
+class Server {
+    readonly app: express.Express;
 
-    res.setHeader('Server', 'nxapi/' + version + ' ' + product);
-    res.setHeader('X-Server', 'nxapi/' + version + ' ' + product);
-    res.setHeader('X-Served-By', os.hostname());
+    readonly promise = new Map<string, Promise<ReplayData>>();
 
-    next();
-});
+    constructor(
+        readonly splatnet: SplatNet3,
+        readonly cache: persist.LocalStorage,
+    ) {
+        const app = express();
 
-app.get('/api/splatnet3/replay/:code', async (req, res) => {
-    try {
-        const code = req.params.code.replace(/^([A-Z0-9]{4})-([A-Z0-9]{4})-([A-Z0-9]{4})-([A-Z0-9]{4})$/, '$1$2$3$4');
+        app.use('/api/splatnet3', (req, res, next) => {
+            console.log('[%s] %s %s HTTP/%s from %s, port %d%s, %s',
+                new Date(), req.method, req.url, req.httpVersion,
+                req.socket.remoteAddress, req.socket.remotePort,
+                req.headers['x-forwarded-for'] ? ' (' + req.headers['x-forwarded-for'] + ')' : '',
+                req.headers['user-agent']);
+
+            res.setHeader('Server', 'nxapi/' + version + ' ' + product);
+            res.setHeader('X-Server', 'nxapi/' + version + ' ' + product);
+            res.setHeader('X-Served-By', os.hostname());
+
+            next();
+        });
+
+        app.get('/api/splatnet3/replay/:code', this.createApiRequestHandler((req, res) =>
+            this.handleReplayRequest(req, res, req.params.code)));
+
+        this.app = app;
+    }
+
+    protected createApiRequestHandler(callback: (req: Request, res: Response) => Promise<{} | void>) {
+        return async (req: Request, res: Response) => {
+            try {
+                const result = await callback.call(null, req, res);
+
+                if (result) this.sendJsonResponse(res, result);
+                else res.end();
+            } catch (err) {
+                this.handleRequestError(req, res, err);
+            }
+        };
+    }
+
+    protected sendJsonResponse(res: Response, data: {}, status?: number) {
+        if (status) res.statusCode = status;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(this.encodeJsonForResponse(data, res.req.headers['accept']?.match(/\/html\b/i) ? 4 : 0));
+    }
+
+    protected encodeJsonForResponse(data: unknown, space?: number) {
+        return JSON.stringify(data, null, space);
+    }
+
+    protected handleRequestError(req: Request, res: Response, err: unknown) {
+        debug('Error in request %s %s', req.method, req.url, err);
+
+        if (err instanceof ResponseError) {
+            err.sendResponse(req, res);
+        } else {
+            this.sendJsonResponse(res, {
+                error: 'unknown_error',
+                error_message: (err as Error).message,
+                error_data: err,
+            }, 500);
+        }
+    }
+
+    async handleReplayRequest(req: Request, res: Response, code: string) {
+        code = code.replace(/^([A-Z0-9]{4})-([A-Z0-9]{4})-([A-Z0-9]{4})-([A-Z0-9]{4})$/, '$1$2$3$4');
 
         if (!REPLAY_CODE_REGEX.test(code)) {
-            throw new Error('Invalid replay code');
+            throw new ResponseError(400, 'invalid_code', 'Invalid replay code');
         }
 
-        console.warn('Lookup %s', code);
+        debug('Lookup %s', code);
 
-        const promise = replays.get(code) ?? Promise.resolve().then(async () => {
-            const request_id = splatnet.api.getPersistedQueryId(RequestId.DownloadSearchReplayQuery);
+        const data = await this.lookupReplayCode(code);
+
+        if (!data.replay) {
+            throw new ResponseError(404, 'not_found', 'Replay not found');
+        }
+
+        const replay_id = Buffer.from(data.replay.id, 'base64').toString();
+        const match = replay_id.match(/^Replay-(u-[a-z0-9]{20}):([A-Z0-9]{16})$/);
+        if (!match) throw new ResponseError(500, 'unknown_error', 'Error decoding replay ID');
+        const npln_user_id = match[1];
+
+        return {
+            replay: data.replay,
+            request_id: data.request_id,
+            npln_user_id,
+        };
+    }
+
+    lookupReplayCode(code: string) {
+        const promise = this.promise.get(code) ?? Promise.resolve().then(async () => {
+            const request_id = this.splatnet.api.getPersistedQueryId(RequestId.DownloadSearchReplayQuery);
             const key = 'Replay.' + request_id + '.' + code;
-            const cache_replay: ReplayData | undefined = await cache.getItem(key);
+            const cache_replay: ReplayData | undefined = await this.cache.getItem(key);
 
-            if (cache_replay) return cache_replay;
+            if (cache_replay) {
+                debug('Using cached data for replay code %s', code);
+                return cache_replay;
+            }
 
             try {
-                const replay = await splatnet.api.getReplaySearchResult(code);
+                debug('Searching for replay code %s', code);
+                const replay = await this.splatnet.api.getReplaySearchResult(code);
 
                 const data: ReplayData = {
                     replay: replay.data.replay,
-                    request_id,
+                    request_id: replay[RequestIdSymbol],
                     created_at: Date.now(),
                 };
 
-                await cache.setItem(key, data);
+                await this.cache.setItem(key, data);
 
                 return data;
             } catch (err) {
@@ -82,52 +147,57 @@ app.get('/api/splatnet3/replay/:code', async (req, res) => {
                         created_at: Date.now(),
                     };
 
-                    await cache.setItem(key, data);
+                    await this.cache.setItem(key, data);
 
                     return data;
                 }
 
                 throw err;
             }
-        }).finally(() => {
-            replays.delete(code);
+        }).then(result => {
+            this.promise.delete(code);
+            return result;
+        }).catch(err => {
+            this.promise.delete(code);
+            throw err;
         });
 
-        replays.set(code, promise);
+        this.promise.set(code, promise);
 
-        const data = await promise;
-
-        if (!data.replay) {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                error: 'not_found',
-                error_message: 'Replay not found',
-            }, null, 4) + '\n');
-            return;
-        }
-
-        const replay_id = Buffer.from(data.replay.id, 'base64').toString();
-        const match = replay_id.match(/^Replay-(u-[a-z0-9]{20}):([A-Z0-9]{16})$/);
-        if (!match) throw new Error('Error decoding replay ID');
-        const npln_user_id = match[1];
-
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({
-            replay: data.replay,
-            request_id: data.request_id,
-            npln_user_id,
-        }, null, 4) + '\n');
-    } catch (err) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({
-            error: err,
-            error_message: (err as Error).message,
-        }, null, 4) + '\n');
+        return promise;
     }
-});
+}
 
-app.listen(8080, () => {
+export class ResponseError extends Error {
+    constructor(readonly status: number, readonly code: string, message?: string) {
+        super(message);
+    }
+
+    sendResponse(req: Request, res: Response) {
+        const data = {
+            error: this.code,
+            error_message: this.message,
+        };
+
+        res.statusCode = this.status;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(req.headers['accept']?.match(/\/html\b/i) ?
+            JSON.stringify(data, null, 4) : JSON.stringify(data));
+    }
+}
+
+const storage = await Storage.create(LocalStorageProvider, new URL('../data', import.meta.url));
+const users = new Users(storage, process.env.ZNC_PROXY_URL);
+const splatnet = await users.get(SplatNet3, process.env.NA_USER_ID!);
+
+const cache = persist.create({
+    dir: fileURLToPath(new URL('../data/persist', import.meta.url)),
+    stringify: data => JSON.stringify(data, null, 4) + '\n',
+});
+await cache.init();
+
+const server = new Server(splatnet, cache);
+
+server.app.listen(8080, () => {
     console.log('Listening on [::]:8080');
 });
